3da0babfddb4c2b441789b091f4ecd06ef494452
sailie-ghole <gholesailie1410@gmail.com>
2023-09-28 09:07:35 +0530

    Inital

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 5555;
+
+//add mongodb and mongoose to nodejs
+export const mongoDBURL =
+    'mongodb+srv://sailieg14:priyanka2414@book-store-mern.jlgty7z.mongodb.net/?retryWrites=true&w=majority';
+
